b8183da2b1379db2f1e1da41aec2ec1e38085325
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-03-11 20:17:36 +0100

    plot

--- src/plot2.py
+++ src/plot2.py
@@ -31,7 +31,7 @@ plt.tick_params(axis="both", which="both", bottom="off", top="off",
 plt.grid(b=True, which='minor', alpha=0.2)
 
 #------------------Plotting-----------------
-x, Xe = np.loadtxt("Xe.dat")
+x, Xe = np.loadtxt("Xe.dat", unpack=True)
 
 # Plotting omegas
 plt.title(r"X_e")
