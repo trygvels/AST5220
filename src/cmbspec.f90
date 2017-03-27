1d40fd6596d72ece03e8c9004ea1bf3b9fb760bd
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-03-27 14:26:20 +0200

    Finishing up monday

--- src/cmbspec.f90
+++ src/cmbspec.f90
@@ -4,10 +4,33 @@ program cmbspec
   use time_mod
   use rec_mod
   implicit none
+  integer(i4b) :: i      ! Number of grid points
 
   ! Initialize time grids
   call initialize_time_mod
+
   call initialize_rec_mod
-  ! Output to file desired quantities here
+
+  ! ------ Output to file desired quantities here ------
+  ! Write to file - x_rec, X_e
+  open(1, file="Xe.dat", action="write",status="replace")
+  open(2, file="neLog.dat", action="write",status="replace") ! Eldens
+  open(3, file="tau.dat", action="write",status="replace") ! taus
+  open(4, file="g.dat", action="write",status="replace") ! visibility
+
+  do i=1, n
+     write(1,*) x_rec(i), X_e(i)
+     write(2,*) n_e(i), n_e2(i)
+     write(3,*) tau(i),tau2(i),tau22(i)
+     write(4,*) g(i), g2(i), g22(i)
+  end do
+
+  close(1)
+  close(2)
+  close(3)
+  close(4)
+
+
+
 
 end program cmbspec
