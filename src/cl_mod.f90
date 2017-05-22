4a44d526c46016c02099362fdf8f98cce35d6dc4
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-05-22 21:43:14 +0200

    Reverted integration to check

--- src/cl_mod.f90
+++ src/cl_mod.f90
@@ -82,35 +82,62 @@ contains
     ! #### C_l COMPUTATION OVER l's ####
 
     do l = 1, l_num
-      integralk = 0.d0 ! Reset k integral
+      !################### METHOD 1 ##################
+      !integralk = 0.d0 ! Reset k integral
+      !do k = 1, k_num
+      !  integralx = 0.d0 ! Reset x integral
+
+      !  ! Compute integrand over x with 1/10th total array
+      !  do i = 1, x_num/10.d0
+      !     ilo = 1 + (i-1)*(x_num-1)/(x_num/10-1) !Speed up integration
+      !     x_lores(i) = x_hires(ilo)
+      !     integrandx(i)=S(ilo,k)*splint(z_spline,j_l(:,l),j_l2(:,l),k_hires(k)*(get_eta(0.d0)-get_eta(x_hires(ilo))))
+      !  end do
+
+      !  ! Trapezoidal integration over x
+      !  do i=1, x_num/10.d0-1
+      !     integralx = integralx + (x_lores(i+1)-x_lores(i))*(integrandx(i+1)+integrandx(i))/2.d0
+      !  end do
+      !  Theta(l,k) = integralx
+      !  ! Compute integrand over k
+      !  integrandk(k) = (c*k_hires(k)/H_0)**(n_s-1.d0)*integralx**2/k_hires(k)
+      !end do
+
+      !! Trapezoidal integration over k
+      !do k=1, k_num-1
+      !   integralk = integralk + (k_hires(k+1)-k_hires(k))*(integrandk(k+1)+integrandk(k))/2.d0
+      !end do
+
+      !! Task: Store C_l in an array. Optionally output to file
+      !cls(l) = integralk*ls(l)*(ls(l)+1.d0)/(2.d0*pi)
+
+      !################### METHOD 2 ##################
+
+      integralk = 0 ! Reset integral for each value of cls
       do k = 1, k_num
-        integralx = 0.d0 ! Reset x integral
+        integralx = 0 ! Reset integral for each value of theta
 
-        ! Compute integrand over x with 1/10th total array
-        do i = 1, x_num/10.d0
-           ilo = 1 + (i-1)*(x_num-1)/(x_num/10-1) !Speed up integration
-           x_lores(i) = x_hires(ilo)
-           integrandx(i)=S(ilo,k)*splint(z_spline,j_l(:,l),j_l2(:,l),k_hires(k)*(get_eta(0.d0)-get_eta(x_hires(ilo))))
+        ! Integrate theta
+        do i = 1, x_num/10
+          ilo = 1 + (i-1)*(x_num-1)/(x_num/10-1) !Speed up integration
+          x_lores(i) = x_hires(ilo)
+          integrandx(i) = S(ilo,k)*splint(z_spline,j_l(:,l),j_l2(:,l),k_hires(k)*(get_eta(0.d0)-get_eta(x_hires(ilo))))
+          integralx = integralx + integrandx(i)
         end do
+        ! Subtract half of first and last integrand for x
+        Theta(l,k) = h1*(integralx - 0.5d0*(integrandx(1)+integrandx(x_num)))
 
-        ! Trapezoidal integration over x
-        do i=1, x_num/10.d0-1
-           integralx = integralx + (x_lores(i+1)-x_lores(i))*(integrandx(i+1)+integrandx(i))/2.d0
-        end do
-        Theta(l,k) = integralx
-        ! Compute integrand over k
-        integrandk(k) = (c*k_hires(k)/H_0)**(n_s-1.d0)*integralx**2/k_hires(k)
+
+        ! Integrate C_l
+        integrandk(k) = (c*k_hires(k)/H_0)**(n_s-1.d0)*Theta(l,k)**2/k_hires(k)
+        integralk = integralk + integrandk(k)
       end do
 
-      ! Trapezoidal integration over k
-      do k=1, k_num-1
-         integralk = integralk + (k_hires(k+1)-k_hires(k))*(integrandk(k+1)+integrandk(k))/2.d0
-      end do
+      ! Subtract half of first and last integrand for k
+      integralk = h2*(integralk - 0.5d0*(integrandk(1)+integrandk(k_num)))
 
-      ! Task: Store C_l in an array. Optionally output to file
+      ! Store C_l in an array. Optionally output to file
       cls(l) = integralk*ls(l)*(ls(l)+1.d0)/(2.d0*pi)
-
-
        !write the transfer function to file
       ! if(ls(l)==2) then
       !     do k=1,k_num
