b1d2a2c22e7784587e97c8ad8e4b8452cdce2e84
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-05-22 21:10:13 +0200

    Finished variable data set

--- src/cl_mod.f90
+++ src/cl_mod.f90
@@ -59,12 +59,12 @@ contains
     end do
 
     ! Open files to write transfer functions
-    open(unit=123, file=fileplace//"integrand1.dat", action="write", status="replace")
-    open(unit=124, file=fileplace//"integrand2.dat", action="write", status="replace")
-    open(unit=125, file=fileplace//"integrand3.dat", action="write", status="replace")
-    open(unit=126, file=fileplace//"integrand4.dat", action="write", status="replace")
-    open(unit=127, file=fileplace//"integrand5.dat", action="write", status="replace")
-    open(unit=128, file=fileplace//"integrand6.dat", action="write", status="replace")
+    !open(unit=123, file=fileplace//"integrand1.dat", action="write", status="replace")
+    !open(unit=124, file=fileplace//"integrand2.dat", action="write", status="replace")
+    !open(unit=125, file=fileplace//"integrand3.dat", action="write", status="replace")
+    !open(unit=126, file=fileplace//"integrand4.dat", action="write", status="replace")
+    !open(unit=127, file=fileplace//"integrand5.dat", action="write", status="replace")
+    !open(unit=128, file=fileplace//"integrand6.dat", action="write", status="replace")
 
     !Spline bessel functions, get second derivative for later splint
     do l=1,l_num
@@ -112,42 +112,42 @@ contains
 
 
        !write the transfer function to file
-       if(ls(l)==2) then
-           do k=1,k_num
-               write (123,'(*(2X, ES14.6E3))') c*k_hires(k)/H_0 , ls(l)*(ls(l)+1.d0)*&
-                     Theta(l,k)**2/(c*k_hires(k)/H_0)
-           end do
-       end if
-       if(ls(l)==50) then
-           do k=1,k_num
-               write (124,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
-                     /(c*k_hires(k)/H_0)
-           end do
-       end if
-       if(ls(l)==200) then
-           do k=1,k_num
-               write (125,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
-                     /(c*k_hires(k)/H_0)
-           end do
-       end if
-       if(ls(l)==500) then
-           do k=1,k_num
-               write (126,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
-                     /(c*k_hires(k)/H_0)
-           end do
-       end if
-       if(ls(l)==800) then
-           do k=1,k_num
-               write (127,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
-                     /(c*k_hires(k)/H_0)
-           end do
-       end if
-       if(ls(l)==1200) then
-           do k=1,k_num
-               write (128,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
-                     /(c*k_hires(k)/H_0)
-           end do
-       end if
+      ! if(ls(l)==2) then
+      !     do k=1,k_num
+      !         write (123,'(*(2X, ES14.6E3))') c*k_hires(k)/H_0 , ls(l)*(ls(l)+1.d0)*&
+      !               Theta(l,k)**2/(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
+      ! if(ls(l)==50) then
+      !     do k=1,k_num
+      !         write (124,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
+      !               /(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
+      ! if(ls(l)==200) then
+      !     do k=1,k_num
+      !         write (125,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
+      !               /(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
+      ! if(ls(l)==500) then
+      !     do k=1,k_num
+      !         write (126,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
+      !               /(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
+      ! if(ls(l)==800) then
+      !     do k=1,k_num
+      !         write (127,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
+      !               /(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
+      ! if(ls(l)==1200) then
+      !     do k=1,k_num
+      !         write (128,'(*(2X, ES14.6E3))') ls(l)*(ls(l)+1.d0)*Theta(l,k)**2 &
+      !               /(c*k_hires(k)/H_0)
+      !     end do
+      ! end if
 
      ! Timer for loop
        call cpu_time(finish)
@@ -156,12 +156,12 @@ contains
     end do
 
     ! Close open data files
-    close(123)
-    close(124)
-    close(125)
-    close(126)
-    close(127)
-    close(128)
+    !close(123)
+    !close(124)
+    !close(125)
+    !close(126)
+    !close(127)
+    !close(128)
 
     ! Convert ls to double precision
     allocate(ls_dp(l_num))
