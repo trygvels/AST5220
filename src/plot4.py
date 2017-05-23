c4a9a4901b104b103b70a04fd709fc288008b765
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-05-24 01:33:56 +0200

    Only need parameter estimation and k values

--- src/plot4.py
+++ src/plot4.py
@@ -27,6 +27,7 @@ fig.savefig('figs/xkcdcl.pdf', bbox_inches='tight',pad_inches=0.106)
 plt.show()
 
 """
+
 #-------------------- Colors --------------------
 # These are the "Tableau 20" colors as RGB.
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
@@ -37,8 +38,59 @@ tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
 for i in range(len(tableau20)):
     r, g, b = tableau20[i]
     tableau20[i] = (r / 255., g / 255., b / 255.)
+#--------------------CL change h --------------------
+fig = plt.figure()
+# Remove the plot frame lines. They are unnecessary chartjunk.
+ax = plt.subplot(111)
+ax.yaxis.grid()
+#ax.xaxis.grid()
+ax.spines["top"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_visible(False)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+# Remove the tick marks; they are unnecessary with the tick lines we just plotted.
+plt.tick_params(axis="both", which="both", bottom="off", top="off",
+                labelbottom="on", left="off", right="off", labelleft="on")
+
+plt.grid(b=True, which='minor', alpha=0.2)
+
+planck1  = np.loadtxt("data/COM_PowerSpect_CMB-TT-loL-full_R2.02.txt",unpack=True,skiprows=3)
+planck2  = np.loadtxt("data/COM_PowerSpect_CMB-TT-hiL-full_R2.02.txt",unpack=True,skiprows=3)
+
+planck_l1 = planck1[0]
+planck_l2 = planck2[0]
+
+C_llow = planck1[1]
+C_lhi  = planck2[1]
+
+error1 = planck1[2]
+error2 = planck2[2]
+
+Clplanck = np.hstack([C_llow,C_lhi])
+planck_l = np.hstack([planck_l1,planck_l2])
+error = np.hstack([error1,error2])
 
 
+
+#lhi,clhiBest = np.loadtxt("data/C_lBest.dat", unpack=True)
+lhi,clhi = np.loadtxt("data/C_l.dat", unpack=True)
+
+plt.xlabel(r'l')
+plt.ylabel(r'$l(l+1)C_l/2\pi$')
+plt.xlim(0,1200)
+plt.ylim(0,6000)
+K = 5775
+#clhiBest = clhiBest/max(clhiBest)*K
+clhi = clhi/max(clhi[100:])*K
+#plt.plot(lhi, clhiBest,label=r"$C_l$ Best fit", color=tableau20[2],zorder=2)
+plt.plot(lhi, clhi,label=r"$C_l$ Default", color=tableau20[4],zorder=4)
+plt.errorbar(planck_l,Clplanck,yerr=error,label='Planck data',color=tableau20[15],zorder=1)#,fmt='-o')
+plt.legend(fancybox=True,framealpha=0.5,loc=1)
+#fig.savefig('figs/cl.pdf', bbox_inches='tight',pad_inches=0.106)
+plt.show()
+"""
 #--------------------CL change h --------------------
 fig = plt.figure()
 # Remove the plot frame lines. They are unnecessary chartjunk.
@@ -205,6 +257,43 @@ plt.legend(fancybox=True,framealpha=0.5,loc=1)
 fig.savefig('figs/clvarm.pdf', bbox_inches='tight',pad_inches=0.106)
 plt.show()
 
+"""
+#-------------------- Transfer -------------------
+"""
+fig = plt.figure()
+# Remove the plot frame lines. They are unnecessary chartjunk.
+ax = plt.subplot(111)
+ax.yaxis.grid()
+#ax.xaxis.grid()
+ax.spines["top"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_visible(False)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+# Remove the tick marks; they are unnecessary with the tick lines we just plotted.
+plt.tick_params(axis="both", which="both", bottom="off", top="off",
+                labelbottom="on", left="off", right="off", labelleft="on")
+
+plt.grid(b=True, which='minor', alpha=0.2)
+
+trans1 = np.loadtxt("data/transfer1.dat", unpack=True)
+trans2 = np.loadtxt("data/transfer2.dat", unpack=True)
+trans3 = np.loadtxt("data/transfer3.dat", unpack=True)
+trans4 = np.loadtxt("data/transfer4.dat", unpack=True)
+trans5 = np.loadtxt("data/transfer5.dat", unpack=True)
+
+plt.ylim(-0.0025,0.0025)
+plt.xlabel(r'l')
+plt.ylabel(r'$\Theta_l(k)$')
+plt.plot(lhi,trans1[1:], color=tableau20[0])
+plt.plot(lhi,trans2[1:], color=tableau20[2])
+plt.plot(lhi,trans3[1:], color=tableau20[4])
+plt.plot(lhi,trans4[1:], color=tableau20[6])
+plt.plot(lhi,trans5[1:], color=tableau20[8])
+fig.savefig('figs/transfer.pdf', bbox_inches='tight',pad_inches=0.106)
+plt.show()
+
 #----------------INTEGRANDS----------------
 fig = plt.figure()
 # Remove the plot frame lines. They are unnecessary chartjunk.
@@ -223,23 +312,20 @@ plt.tick_params(axis="both", which="both", bottom="off", top="off",
 
 plt.grid(b=True, which='minor', alpha=0.2)
 
-khi,thetahi1 = np.loadtxt("data/integrand1.dat", unpack=True)
-thetahi2 = np.loadtxt("data/integrand2.dat", unpack=True)
-thetahi3 = np.loadtxt("data/integrand3.dat", unpack=True)
-thetahi4 = np.loadtxt("data/integrand4.dat", unpack=True)
-thetahi5 = np.loadtxt("data/integrand5.dat", unpack=True)
-thetahi6 = np.loadtxt("data/integrand6.dat", unpack=True)
-
-plt.xlabel(r'k*c/H_0')
-plt.ylabel(r'$l(l+1)\Theta^2/(ck/H_0)$')
-plt.xlim([0,500])
-plt.ylim(0,0.08)
-plt.plot(khi, thetahi1,label=r"$\Theta, l = 2$", color=tableau20[0])
-plt.plot(khi, thetahi2,label=r"$\Theta, l = 50$", color=tableau20[1])
-plt.plot(khi, thetahi3,label=r"$\Theta, l = 200$", color=tableau20[2])
-plt.plot(khi, thetahi4,label=r"$\Theta, l = 500$", color=tableau20[3])
-plt.plot(khi, thetahi5,label=r"$\Theta, l = 800$", color=tableau20[4])
-plt.plot(khi, thetahi6,label=r"$\Theta, l = 1200$", color=tableau20[5])
+integrand1 = trans1[1:]**2/trans1[0]
+integrand2 = trans2[1:]**2/trans2[0]
+integrand3 = trans3[1:]**2/trans3[0]
+integrand4 = trans4[1:]**2/trans4[0]
+integrand5 = trans5[1:]**2/trans5[0]
+plt.ylim(1e-18,1e10)
+plt.xlabel(r'l')
+plt.ylabel(r'$\Theta^2(k)/k$')
+plt.semilogy(lhi, integrand1,label=r"$k = 0H_0/c$", color=tableau20[0])
+plt.semilogy(lhi, integrand2,label=r"$k = 1000H_0/c$", color=tableau20[1])
+plt.semilogy(lhi, integrand3,label=r"$k = 2000H_0/c", color=tableau20[2])
+plt.semilogy(lhi, integrand4,label=r"$k = 3000H_0/c$", color=tableau20[3])
+plt.semilogy(lhi, integrand5,label=r"$k = 4000H_0/c$", color=tableau20[4])
 plt.legend(fancybox=True,framealpha=0.5,loc=7)
-fig.savefig('figs/integrands.pdf', bbox_inches='tight',pad_inches=0.106)
+fig.savefig('figs/integrand.pdf', bbox_inches='tight',pad_inches=0.106)
 plt.show()
+"""
